10a70a0f004b777d00b48f6286a71471b8d134c5
Muhammad Nafees <nf10018@gmail.com>
2024-12-16 02:45:59 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(cors({origin: true, credentials: true}));
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+ // app.use(express.static("public"));
 
 export default app;
